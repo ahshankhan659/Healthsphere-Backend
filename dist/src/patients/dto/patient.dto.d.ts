b2de12b8658484025529b2117f966ec76e9d8892
ahshankhan659 <akahshankhan659@gmail.com>
2026-07-06 19:56:06 +0500

    Remove node_modules, and .env from repository

--- dist/src/patients/dto/patient.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class UpdatePatientDto {
-    fullName?: string;
-    phone?: string;
-    address?: string;
-    allergies?: string[];
-    chronicConditions?: string[];
-    bloodType?: string;
-    emergencyContact?: string;
-}
